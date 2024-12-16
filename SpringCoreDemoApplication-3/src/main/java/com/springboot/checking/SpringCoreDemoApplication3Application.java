9d68e67b3add970c3c976746f766fbbf5a603169
PradeepVemula96 <vemulapradeep96ab@gmail.com>
2024-12-16 10:43:49 +0530

    spring related

---
+++ SpringCoreDemoApplication-3/src/main/java/com/springboot/checking/SpringCoreDemoApplication3Application.java
@@ -0,0 +1,13 @@
+package com.springboot.checking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCoreDemoApplication3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCoreDemoApplication3Application.class, args);
+	}
+
+}
---
+++ SpringCoreDemoApplication-3/src/test/java/com/springboot/checking/SpringCoreDemoApplication3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.checking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCoreDemoApplication3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
